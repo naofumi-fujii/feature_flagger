f89fd16b93a2475c4f5eb5d09e7a8f1961932834
naofumi-fujii <naofumi.fujii@gmail.com>
2019-12-12 13:28:51 +0900

    super simple usage with code

--- lib/feature_flagger.rb
+++ lib/feature_flagger.rb
@@ -2,5 +2,16 @@ require "feature_flagger/version"
 
 module FeatureFlagger
   class Error < StandardError; end
+
   # Your code goes here...
+
+  # USAGE
+  #
+  # FeatureFlagger.feature active_if: true do
+  #   puts :hoge
+  # end
+
+  def self.feature(active_if:)
+    yield if active_if
+  end
 end
